fdccd0fbff879f8a12d4dc0f6329c31f40d3e63b
Denis V. Ryabokon <ryabokon_dv@bw-sw.com>
2018-10-05 16:06:07 +0700

    add annotation.conf, fix converter runner

--- brat_converter/text-to-brat.sh
+++ brat_converter/text-to-brat.sh
@@ -2,8 +2,9 @@
 #
 # Usage: ./text-to-brat.sh ./data your.json OilAndGasTextPreprocessor ./model/oil_and_gas_keywords.txt brat-out
 #
+CONVERTER_FOLDER=brat_converter
 
 python console.py training -s $1 -d $2 -p $3 -k $4 --spacy en_core_web_lg
-python json2brat.py --source $2 --destination $5 --ann-conf annotation.conf
+python $CONVERTER_FOLDER/json2brat.py --source $2 --destination $5 --ann-conf $CONVERTER_FOLDER/annotation.conf
 
 echo "Ready!"
